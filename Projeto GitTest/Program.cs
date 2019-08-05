228655cde11c9c47621385550bf1917c69aad07b
Fernanda Ek <fernandaek@icloud.com>
2019-08-05 17:47:21 +0200

    Testing commit from Bash

---
+++ Projeto GitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
